be601cf42cfce7b2c158026e416e3265c62ce559
Valentin <v7337337@gmail.com>
2022-10-09 20:12:13 +0300

    Add files via upload
    Lab number one

---
+++ Lab_1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	
+	system ("chcp 65001");
+	
+	printf("Введите координаты x и y: \n");
+	
+	float x = 0, y = 0;
+	
+	scanf ("%f%f", &x, &y);
+	
+	if (y >= 2 / 3 * x - 2 && y <= -2 / 3 * x + 2 && y <= 2 && y >= -2 && x <= 1.5 && x >= -2)
+		
+		printf ("Точка принадлежит фигуре\n");
+	
+		else 
+	
+			printf ("Точка не принадлежит фигуре\n");
+	
+	system ("PAUSE");
+	
+return 0;
+
+}
